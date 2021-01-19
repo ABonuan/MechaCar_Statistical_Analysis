65b27c7437c9d09c2e66f065ab601b63c3176fef
Anna Bonuan <anna.bonuan@gmail.com>
2021-01-18 18:05:55 -0800

    summary statistics for PSI of supsension coils

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -4,3 +4,9 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle +
      ground_clearance + AWD, data=MechaCar_table) #generate multiple linear regression model
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + 
              ground_clearance + AWD, data=MechaCar_table)) #generate summary statistics
+
+SuspensionCoils_table <- read.csv(file="Suspension_Coil.csv",check.names = F,stringsAsFactors = F)
+total_summary <- SuspensionCoils_table %>% 
+  summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI)) #create summary table
+lot_summary <- SuspensionCoils_table %>% group_by(Manufacturing_Lot) %>%
+  summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups="keep") #create summary table by Lot no.
