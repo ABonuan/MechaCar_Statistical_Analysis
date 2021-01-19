dc23575203c5cb8b52cca442c668ee705b245ced
Anna Bonuan <anna.bonuan@gmail.com>
2021-01-18 20:55:02 -0800

    T-tests of Lots vs Presumed Population Mean

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -10,3 +10,12 @@ total_summary <- SuspensionCoils_table %>%
   summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI)) #create summary table
 lot_summary <- SuspensionCoils_table %>% group_by(Manufacturing_Lot) %>%
   summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups="keep") #create summary table by Lot no.
+
+t.test(log10(SuspensionCoils_table$PSI), mu=1500) #compare PSI of all lots vs presumed population means
+
+t.test(log10(subset(SuspensionCoils_table$PSI,SuspensionCoils_table$Manufacturing_Lot == "Lot1")),
+       mu = 1500) #compare PSI of Lot1 vs presumed population means
+t.test(log10(subset(SuspensionCoils_table$PSI,SuspensionCoils_table$Manufacturing_Lot == "Lot2")),
+       mu = 1500) #compare PSI of Lot2 vs presumed population means
+t.test(log10(subset(SuspensionCoils_table$PSI,SuspensionCoils_table$Manufacturing_Lot == "Lot3")),
+       mu = 1500) #compare PSI of Lot3 vs presumed population means
